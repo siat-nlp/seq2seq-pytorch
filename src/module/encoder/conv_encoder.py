730488a12a6a09d55721696a4f63c1d1b168e77b
ThomasK427 <1935448858@qq.com>
2019-03-22 10:40:34 +0800

    update conv_encoder

--- src/module/encoder/conv_encoder.py
+++ src/module/encoder/conv_encoder.py
@@ -3,48 +3,32 @@ import torch.nn.functional as F
 from torch.nn import init
 import math
 from src.module.encoder.encoder import Encoder
-
+from src.module.utils.clone import clone
+from src.module.utils.constants import PAD_INDEX
 
 class ConvEncoder(Encoder):
 
-    def __init__(self, embedding, hidden_size, kernel_size, num_layers, dropout):
+    def __init__(self, embedding, positional_embedding, layer, num_layers):
         super(ConvEncoder, self).__init__()
-        assert kernel_size % 2 == 1
         self.embedding = embedding
-        self.dropout = dropout
+        self.positional_embedding = positional_embedding
         embed_size = embedding.embedding_dim
+        hidden_size = layer.hidden_size
         self.input_projection = nn.Linear(embed_size, hidden_size)
-        self.convs = nn.ModuleList()
-        for _ in range(num_layers):
-            conv = nn.Conv1d(
-                in_channels=hidden_size,
-                out_channels=2 * hidden_size,
-                kernel_size=kernel_size,
-                padding=kernel_size // 2
-            )
-            std = math.sqrt((4 * (1.0 - dropout)) / (kernel_size * hidden_size))
-            nn.init.normal_(conv.weight, mean=0, std=std)
-            init.constant_(conv.bias, 0)
-            conv = nn.utils.weight_norm(conv, dim=2)
-            self.convs.append(conv)
+        self.layers = clone(layer, num_layers)
         self.output_projection = nn.Linear(hidden_size, embed_size)
 
     def forward(self, src):
-        src_embedding = self.embedding(src)
+        src_embedding = self.embedding(src) + self.positional_embedding(src)
         src_embedding = F.dropout(src_embedding, p=self.dropout, training=self.training)
-        mask = (src != 0).transpose(0, 1)   # ByteTensor (time_step, batch_size)
+        mask = (src != PAD_INDEX)   # ByteTensor (time_step, batch_size)
         src = src_embedding.transpose(0, 1) # (time_step, batch_size, embed_size)
         src = self.input_projection(src)
-        for conv in self.convs:
-            src = src.masked_fill(mask.unsqueeze(-1), 0)
-            residual = src
-            src = F.dropout(src, p=self.dropout, training=self.training)
-            src = conv(src)
-            src = F.glu(src, dim=2)
-            src = (src + residual) * math.sqrt(0.5)
+        for layer in self.layers:
+            src = layer(src)
         src = self.output_projection(src)
-        src = src.masked_fill(mask.unsqueeze(-1), 0)
         src = src.transpose(0, 1)
-        # src = GradMultiply(src, 1.0 / (2.0 * self.num_attention_layers))
         embed_src = (src + src_embedding) * math.sqrt(0.5)
+        src = src.masked_fill(mask.unsqueeze(-1)==0, 0)
+        embed_src = embed_src.masked_fill(mask.unsqueeze(-1)==0, 0)
         return src, embed_src, mask
