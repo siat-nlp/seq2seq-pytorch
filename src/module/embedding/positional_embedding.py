f464f6e88fc3be8fd358396545e018515f75d162
ThomasK427 <1935448858@qq.com>
2019-03-22 10:21:58 +0800

    add positional embedding

--- src/module/embedding/positional_embedding.py
+++ src/module/embedding/positional_embedding.py
@@ -1,36 +1,44 @@
 import torch
 from torch import nn
 from torch.nn import init
+import torch.nn.functional as F
+import math
+from src.module.utils.constants import PAD_INDEX
 
-class LearnedPositionalEmbedding(nn.Module):
+class PositionalEmbedding(nn.Module):
 
-    def __init__(self, num_embeddings, embedding_dim, padding_idx):
-        super(LearnedPositionalEmbedding, self).__init__()
-        self.embeddings = nn.Parameter(
-            torch.FloatTensor(num_embeddings, embedding_dim)
-        )
-        init.normal_(self.embeddings, mean=0, std=0.1)
-        init.constant_(self.embeddings[padding_idx], 0)
+    def __init__(self, num_embeddings, embedding_dim, learnable=False):
+        super(PositionalEmbedding, self).__init__()
         self.num_embeddings = num_embeddings
         self.embedding_dim = embedding_dim
-        self.padding_idx = padding_idx
+        self.learnable = learnable
+        if learnable:
+            self.embeddings = nn.Parameter(
+                torch.FloatTensor(num_embeddings, embedding_dim)
+            )
+            init.normal_(self.embeddings, mean=0, std=0.1)
+        else:
+            self.embeddings = torch.zeros(num_embeddings, embedding_dim)
+            position = torch.arange(0, num_embeddings).unsqueeze(1)
+            div_term = torch.exp(torch.arange(0, embedding_dim, 2) * (- math.log(10000.0) / embedding_dim))
+            self.embeddings[:, 0::2] = torch.sin(position * div_term)
+            self.embeddings[:, 1::2] = torch.cos(position * div_term)
 
-    def forward(self, x):
-        mask = (x == self.padding_idx)
+    def forward(self, x, left_padding=0):
+        """
+        x: LongTensor (batch_size, time_step)
+        """
+        x = x[:, left_padding:]
+        x = self._get_embedding(x)
+        x = F.pad(x, [0, 0, left_padding, 0, 0, 0])
+        return x
+
+    def _get_embedding(self, x):
+        mask = (x == PAD_INDEX)
         batch_size, time_step = x.size()
-        start = self.padding_idx + 1
-        end = start + time_step
-        x = torch.arange(start, end).unsqueeze(0).expand(batch_size, time_step).cuda()
-        x = x.masked_fill(mask, self.padding_idx)
+        x = torch.arange(0, time_step).unsqueeze(0).expand(batch_size, time_step).cuda()
         x = x.view(-1)
         x = self.embeddings.index_select(dim=0, index=x)
         x = x.view(batch_size, time_step, self.embedding_dim)
+        x = x.masked_fill(mask.unsqueeze(-1) == 0, 0)
         return x
-
-class FixedPositionalEmbedding(nn.Module):
-
-    def __init__(self):
-        super(FixedPositionalEmbedding).__init__()
-
-    def forward(self, *input):
-        pass
