8264efb050270726017c0a5d5d820777f6327855
ThomasK427 <1935448858@qq.com>
2019-03-24 21:06:50 +0800

    update

--- src/module/encoder/conv_encoder.py
+++ src/module/encoder/conv_encoder.py
@@ -5,9 +5,6 @@ import math
 from src.module.encoder.encoder import Encoder
 from src.module.utils.clone import clone
 from src.module.utils.constants import PAD_INDEX
-from src.module.layer.feed_forward import FeedForward
-from src.module.layer.conv_glu import ConvGLU
-from src.module.layer.conv_relu import ConvReLU
 
 class ConvEncoder(Encoder):
 
@@ -19,6 +16,7 @@ class ConvEncoder(Encoder):
         hidden_size = layer.hidden_size
         self.input_projection = nn.Linear(embed_size, hidden_size)
         self.layers = clone(layer, num_layers)
+        self.layer_norm = nn.LayerNorm(hidden_size)
         self.output_projection = nn.Linear(hidden_size, embed_size)
         self.dropout = dropout
 
@@ -30,6 +28,7 @@ class ConvEncoder(Encoder):
         src = self.input_projection(src)
         for layer in self.layers:
             src = layer(src)
+        src = self.layer_norm(src)
         src = self.output_projection(src)
         src = src.transpose(0, 1)
         embed_src = (src + src_embedding) * math.sqrt(0.5)
@@ -39,32 +38,20 @@ class ConvEncoder(Encoder):
 
 class ConvEncoderLayer(nn.Module):
 
-    def __init__(self, hidden_size, kernel_size, dropout, activatity='glu'):
+    def __init__(self, hidden_size, conv, feed_forward, dropout):
         super(ConvEncoderLayer, self).__init__()
         self.layer_norm1 = nn.LayerNorm(hidden_size)
-        if activatity == 'glu':
-            self.conv = ConvGLU(
-                input_size=hidden_size,
-                output_size=hidden_size,
-                kernel_size=kernel_size,
-                encode=True
-            )
-        else:
-            self.conv = ConvReLU(
-                input_size=hidden_size,
-                output_size=hidden_size,
-                kernel_size=kernel_size,
-                encode=True
-            )
+        self.conv = conv
         self.dropout1 = nn.Dropout(dropout)
         self.layer_norm2 = nn.LayerNorm(hidden_size)
-        self.feed_forward = FeedForward(
-            hidden_size=hidden_size,
-            feed_forward_size=2 * hidden_size
-        )
+        self.feed_forward = feed_forward
         self.dropout2 = nn.Dropout(dropout)
 
-    def forward(self, x):
-        x = self.dropout1(self.conv(self.layer_norm1(x)))
-        x = self.dropout2(self.feed_forward(self.layer_norm2(x)))
-        return x
+    def forward(self, src):
+        src = self.layer_norm1(src)
+        src = self.conv(src)
+        src = self.dropout1(src)
+        src = self.layer_norm2(src)
+        src = self.feed_forward(src)
+        src = self.dropout2(src)
+        return src
