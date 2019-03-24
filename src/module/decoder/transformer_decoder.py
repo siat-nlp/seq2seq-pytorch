3e7c3181f92b03670cdf3ae0de3db3e2920dc585
ThomasK427 <1935448858@qq.com>
2019-03-24 16:01:51 +0800

    update

--- src/module/decoder/transformer_decoder.py
+++ src/module/decoder/transformer_decoder.py
@@ -1,9 +1,19 @@
+from torch import nn
 from src.module.decoder.decoder import Decoder
+from src.module.utils.clone import clone
 
 class TransformerDecoder(Decoder):
 
-    def __init__(self):
+    def __init__(self, embedding, positional_embedding, layer, num_layers, dropout):
         super(TransformerDecoder, self).__init__()
+        self.embedding = embedding
+        self.positional_embedding = positional_embedding
+        embed_size = embedding.embedding_dim
+        hidden_size = layer.hidden_size
+        self.input_projection = nn.Linear(embed_size, hidden_size)
+        self.layers = clone(layer, num_layers)
+        self.output_projection = nn.Linear(hidden_size, embed_size)
+        self.dropout = dropout
 
-    def forward(self, *input):
+    def forward(self, src):
         pass
--- src/module/encoder/transformer_encoder.py
+++ src/module/encoder/transformer_encoder.py
@@ -1,13 +1,26 @@
 from torch import nn
+import torch.nn.functional as F
 from src.module.encoder.encoder import Encoder
+from src.module.utils.clone import clone
 
 class TransformerEncoder(Encoder):
 
-    def __init__(self):
+    def __init__(self, embedding, positional_embedding, layer, num_layers, dropout):
         super(TransformerEncoder, self).__init__()
+        self.embedding = embedding
+        self.positional_embedding = positional_embedding
+        embed_size = embedding.embedding_dim
+        hidden_size = layer.hidden_size
+        self.input_projection = nn.Linear(embed_size, hidden_size)
+        self.layers = clone(layer, num_layers)
+        self.layer_norm = nn.LayerNorm(hidden_size)
+        self.output_projection = nn.Linear(hidden_size, embed_size)
+        self.dropout = dropout
 
     def forward(self, src):
-        pass
+        src_embedding = self.embedding(src) + self.positional_embedding(src)
+        src_embedding = F.dropout(src_embedding, p=self.dropout, training=self.training)
+
 
 class EncoderLayer(nn.Module):
 
