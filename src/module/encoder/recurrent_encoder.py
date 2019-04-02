611fd6297792287c32e0b4abd550b5583bb9fec9
ThomasK427 <1935448858@qq.com>
2019-04-02 14:07:22 +0800

    update

--- src/module/encoder/recurrent_encoder.py
+++ src/module/encoder/recurrent_encoder.py
@@ -23,7 +23,9 @@ class RecurrentEncoder(Encoder):
         src_embedding = F.dropout(src_embedding, p=self.dropout, training=self.training)
         src_mask = (src != PAD_INDEX)
         src_lens = src_mask.long().sum(dim=1, keepdim=False)
-        src = src_embedding
+        src_lens, sort_index = src_lens.sort(descending=True)
+        src_embedding = src_embedding.index_select(index=sort_index, dim=0)
+        packed_src = pack_padded_sequence(src_embedding, src_lens, batch_first=True)
         for layer in self.layers:
             src = layer(src)
         src = self.layer_norm(src)
@@ -31,8 +33,26 @@ class RecurrentEncoder(Encoder):
 
 class RecurrentEncoderLayer(nn.Module):
 
-    def __init__(self):
+    def __init__(self, rnn, feed_forward, dropout):
         super(RecurrentEncoderLayer, self).__init__()
+        hidden_size = rnn.hidden_size
+        self.layer_norm1 = nn.LayerNorm(hidden_size)
+        self.rnn = rnn
+        self.dropout1 = nn.Dropout(dropout)
+        self.layer_norm2 = nn.LayerNorm(hidden_size)
+        self.feed_forward = feed_forward
+        self.dropout2 = nn.Dropout(dropout)
 
-    def forward(self, x):
-        pass
+    def forward(self, src, initial_state):
+        src, src_lens = pad_packed_sequence(src, batch_first=True)
+        src = self.layer_norm1(src)
+        residual = src
+        src = pack_padded_sequence(src, src_lens, batch_first=True)
+        src, final_states = self.rnn(src, initial_state)
+        src, _ = pad_packed_sequence(src, batch_first=True)
+        src = src + residual
+        src = self.dropout1(src)
+        src = self.layer_norm2(src)
+        src = src + self.feed_forward(src)
+        src = self.dropout2(src)
+        return src, final_states
--- src/module/layer/residual_lstm.py
+++ src/module/layer/residual_lstm.py
@@ -1,9 +1,53 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
+from src.module.utils.clone import clone
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 class ResidualLSTM(nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, hidden_size, num_layers=1, bias=True, batch_first=False, dropout=0, bidirectional=True):
         super(ResidualLSTM, self).__init__()
+        self.lstm_layers = clone(nn.LSTM(
+            input_size=hidden_size,
+            hidden_size=hidden_size,
+            num_layers=1,
+            bias=bias,
+            batch_first=batch_first,
+            bidirectional=bidirectional
+        ), num_layers)
+        self.input_projection = nn.Linear(input_size, hidden_size)
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bias = bias
+        self.batch_first = batch_first
+        self.dropout = dropout
+        self.bidirectional = bidirectional
 
-    def forward(self, *input):
-        pass
+    def forward(self, input, initial_states):
+        state_layers = self.num_layers * (2 if self.bidirectional else 1)
+        assert state_layers == initial_states.size(0)
+        initial_states = torch.chunk(initial_states, chunks=self.num_layers, dim=0)
+        input, input_lens = pad_packed_sequence(input, self.batch_first)
+        input = self.input_projection(input)
+        hidden_states = []
+        cell_states = []
+        for i, lstm_layer in self.lstm_layers:
+            residual = input
+            input = pack_padded_sequence(input, input_lens, self.batch_first)
+            initial_state = initial_states[i]
+            output, final_state = lstm_layer(input, initial_state)
+            hidden_state, cell_state = final_state
+            hidden_states.append(hidden_state)
+            cell_states.append(cell_state)
+            output, _ = pad_packed_sequence(output, self.batch_first)
+            output = residual + output
+            if i < self.num_layers - 1:
+                output = F.dropout(output, p=self.dropout, training=self.training)
+            input = output
+        output = pack_padded_sequence(output, input_lens, self.batch_first)
+        hidden_states = torch.cat(hidden_states, dim=0)
+        cell_states = torch.cat(cell_states, dim=0)
+        final_states = (hidden_states, cell_states)
+        return output, final_states
