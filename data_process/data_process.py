e20dae87d2a9a553f531a88ee1114564e8693858
ThomasK427 <1935448858@qq.com>
2019-04-10 16:25:18 +0800

    update

--- data_process/data_process.py
+++ data_process/data_process.py
@@ -3,84 +3,112 @@ import yaml
 import numpy as np
 import pickle
 from data_process.vocab import Vocab
-from data_process.tokenizer import fair_tokenizer, nltk_tokenizer, spacy_en_tokenizer, spacy_de_tokenizer
-from data_process.utils import text_file2word_lists, word_lists2numpy
+from data_process.utils import get_word_lists, word_lists2numpy, parse_path
 
 def data_process(config):
-    if config['tokenizer'] == 'fair':
-        src_tokenizer = fair_tokenizer
-        trg_tokenizer = fair_tokenizer
-    elif config['tokenizer'] == 'nltk':
-        src_tokenizer = nltk_tokenizer
-        trg_tokenizer = nltk_tokenizer
-    elif config['tokenizer'] == 'spacy':
-        src_tokenizer = spacy_de_tokenizer
-        trg_tokenizer = spacy_en_tokenizer
+    path = parse_path(config['data_process']['base_path'])
+    src_train_word_lists = get_word_lists(path['raw']['src_train'])
+    trg_train_word_lists = get_word_lists(path['raw']['trg_train'])
+    src_val_word_lists = get_word_lists(path['raw']['src_val'])
+    trg_val_word_lists = get_word_lists(path['raw']['trg_val'])
+    src_test_word_lists = get_word_lists(path['raw']['src_test'])
+    trg_test_word_lists = get_word_lists(path['raw']['trg_test'])
+
+    if config['share_src_trg_vocab']:
+        vocab = Vocab()
+
+        for word_list in src_train_word_lists:
+            vocab.add_list(word_list)
+        for word_list in trg_train_word_lists:
+            vocab.add_list(word_list)
+
+        word2index, index2word = vocab.get_vocab(max_size=config['data_process']['vocab']['max_size'],
+                                                 min_freq=config['data_process']['vocab']['min_freq'])
+        src_train = word_lists2numpy(src_train_word_lists, word2index)
+        trg_train = word_lists2numpy(trg_train_word_lists, word2index)
+        src_val = word_lists2numpy(src_val_word_lists, word2index)
+        trg_val = word_lists2numpy(trg_val_word_lists, word2index)
+        src_test = word_lists2numpy(src_test_word_lists, word2index)
+        trg_test = word_lists2numpy(trg_test_word_lists, word2index)
+
+        if not os.path.exists(os.path.dirname(path['processed']['src_train'])):
+            os.makedirs(os.path.dirname(path['processed']['src_train']))
+
+        np.savez(path['processed']['train'], src=src_train, trg=trg_train)
+        np.savez(path['processed']['val'], src=src_val, trg=trg_val)
+        np.savez(path['processed']['test'], src=src_test, trg=trg_test)
+
+        with open(path['processed']['word2index'], 'wb') as handle:
+            pickle.dump(word2index, handle)
+        with open(path['processed']['index2word'], 'wb') as handle:
+            pickle.dump(index2word, handle)
+        data_log = {
+            'vocab_size': len(index2word),
+            'oov_size': len(word2index) - len(index2word),
+            'train_src_max_len': src_train.shape[1],
+            'train_trg_max_len': trg_train.shape[1],
+            'val_src_max_len': src_val.shape[1],
+            'val_trg_max_len': trg_val.shape[1],
+            'test_src_max_len': src_test.shape[1],
+            'test_trg_max_len': trg_test.shape[1]
+        }
+        if not os.path.exists(os.path.dirname(path['log']['data_log'])):
+            os.makedirs(os.path.dirname(path['log']['data_log']))
+        with open(path['log']['data_log'], 'w') as handle:
+            yaml.safe_dump(data_log, handle, encoding='utf-8', allow_unicode=True, default_flow_style=False)
     else:
-        raise ValueError('No supporting.')
+        src_vocab = Vocab()
+        trg_vocab = Vocab()
 
-    src_train_text = open(config['path']['raw']['src_train'], 'r', encoding='utf-8')
-    trg_train_text = open(config['path']['raw']['trg_train'], 'r', encoding='utf-8')
-    src_val_text = open(config['path']['raw']['src_val'], 'r', encoding='utf-8')
-    trg_val_text = open(config['path']['raw']['trg_val'], 'r', encoding='utf-8')
-    src_test_text = open(config['path']['raw']['src_test'], 'r', encoding='utf-8')
-    trg_test_text = open(config['path']['raw']['trg_test'], 'r', encoding='utf-8')
+        for word_list in src_train_word_lists:
+            src_vocab.add_list(word_list)
+        for word_list in trg_train_word_lists:
+            trg_vocab.add_list(word_list)
 
-    src_train_word_lists = text_file2word_lists(src_train_text, src_tokenizer)
-    trg_train_word_lists = text_file2word_lists(trg_train_text, trg_tokenizer)
-    src_val_word_lists = text_file2word_lists(src_val_text, src_tokenizer)
-    trg_val_word_lists = text_file2word_lists(trg_val_text, trg_tokenizer)
-    src_test_word_lists = text_file2word_lists(src_test_text, src_tokenizer)
-    trg_test_word_lists = text_file2word_lists(trg_test_text, trg_tokenizer)
+        src_word2index, src_index2word = src_vocab.get_vocab(
+            max_size=config['data_process']['vocab']['max_size'],
+            min_freq=config['data_process']['vocab']['min_freq']
+        )
+        trg_word2index, trg_index2word = trg_vocab.get_vocab(
+            max_size=config['data_process']['vocab']['max_size'],
+            min_freq=config['data_process']['vocab']['min_freq']
+        )
 
-    src_vocab = Vocab()
-    trg_vocab = Vocab()
+        src_train = word_lists2numpy(src_train_word_lists, src_word2index)
+        trg_train = word_lists2numpy(trg_train_word_lists, trg_word2index)
+        src_val = word_lists2numpy(src_val_word_lists, src_word2index)
+        trg_val = word_lists2numpy(trg_val_word_lists, trg_word2index)
+        src_test = word_lists2numpy(src_test_word_lists, src_word2index)
+        trg_test = word_lists2numpy(trg_test_word_lists, trg_word2index)
 
-    for word_list in src_train_word_lists:
-        src_vocab.add_list(word_list)
-    for word_list in trg_train_word_lists:
-        trg_vocab.add_list(word_list)
+        if not os.path.exists(os.path.dirname(path['processed']['src_train'])):
+            os.makedirs(os.path.dirname(path['processed']['src_train']))
 
-    src_word2index, src_index2word = src_vocab.get_vocab(
-        max_size=config['vocab']['src']['max_size'],
-        min_freq=config['vocab']['src']['min_freq']
-    )
-    trg_word2index, trg_index2word = trg_vocab.get_vocab(
-        max_size=config['vocab']['trg']['max_size'],
-        min_freq=config['vocab']['trg']['min_freq']
-    )
+        np.savez(path['processed']['train'], src=src_train, trg=trg_train)
+        np.savez(path['processed']['val'], src=src_val, trg=trg_val)
+        np.savez(path['processed']['test'], src=src_test, trg=trg_test)
 
-    src_train = word_lists2numpy(src_train_word_lists, src_word2index)
-    trg_train = word_lists2numpy(trg_train_word_lists, trg_word2index)
-    src_val = word_lists2numpy(src_val_word_lists, src_word2index)
-    trg_val = word_lists2numpy(trg_val_word_lists, trg_word2index)
-    src_test = word_lists2numpy(src_test_word_lists, src_word2index)
-    trg_test = word_lists2numpy(trg_test_word_lists, trg_word2index)
-
-    if not os.path.exists(os.path.dirname(config['path']['processed']['train'])):
-        os.makedirs(os.path.dirname(config['path']['processed']['train']))
-
-    np.savez(config['path']['processed']['train'], src=src_train, trg=trg_train)
-    np.savez(config['path']['processed']['val'], src=src_val, trg=trg_val)
-    np.savez(config['path']['processed']['test'], src=src_test, trg=trg_test)
-
-    with open(config['path']['processed']['src_word2index'], 'wb') as handle:
-        pickle.dump(src_word2index, handle)
-    with open(config['path']['processed']['src_index2word'], 'wb') as handle:
-        pickle.dump(src_index2word, handle)
-    with open(config['path']['processed']['trg_word2index'], 'wb') as handle:
-        pickle.dump(trg_word2index, handle)
-    with open(config['path']['processed']['trg_index2word'], 'wb') as handle:
-        pickle.dump(trg_index2word, handle)
-    log = {
-        'src_vocab_size': len(src_index2word),
-        'trg_vocab_size': len(trg_index2word),
-        'train_src_max_len': src_train.shape[1],
-        'train_trg_max_len': trg_train.shape[1],
-        'val_src_max_len': src_val.shape[1],
-        'val_trg_max_len': trg_val.shape[1],
-        'test_src_max_len': src_test.shape[1],
-        'test_trg_max_len': trg_test.shape[1]
-    }
-    with open(config['path']['processed']['log_path'], 'w') as handle:
-        yaml.safe_dump(log, handle, encoding='utf-8', allow_unicode=True, default_flow_style=False)
+        with open(path['processed']['src_word2index'], 'wb') as handle:
+            pickle.dump(src_word2index, handle)
+        with open(path['processed']['src_index2word'], 'wb') as handle:
+            pickle.dump(src_index2word, handle)
+        with open(path['processed']['trg_word2index'], 'wb') as handle:
+            pickle.dump(trg_word2index, handle)
+        with open(path['processed']['trg_index2word'], 'wb') as handle:
+            pickle.dump(trg_index2word, handle)
+        data_log = {
+            'src_vocab_size': len(src_index2word),
+            'src_oov_size': len(src_word2index) - len(src_index2word),
+            'trg_vocab_size': len(trg_index2word),
+            'trg_oov_size': len(trg_word2index) - len(trg_index2word),
+            'train_src_max_len': src_train.shape[1],
+            'train_trg_max_len': trg_train.shape[1],
+            'val_src_max_len': src_val.shape[1],
+            'val_trg_max_len': trg_val.shape[1],
+            'test_src_max_len': src_test.shape[1],
+            'test_trg_max_len': trg_test.shape[1]
+        }
+        if not os.path.exists(os.path.dirname(path['log']['data_log'])):
+            os.makedirs(os.path.dirname(path['log']['data_log']))
+        with open(path['log']['data_log'], 'w') as handle:
+            yaml.safe_dump(data_log, handle, encoding='utf-8', allow_unicode=True, default_flow_style=False)
--- data_process/utils.py
+++ data_process/utils.py
@@ -1,10 +1,13 @@
+import os
 import numpy as np
 from src.module.utils.constants import UNK, SOS, EOS
+from data_process.tokenizer import fair_tokenizer
 
-def text_file2word_lists(text_file, tokenizer):
+def get_word_lists(path):
+    text_file = open(path, 'r', encoding='utf-8')
     word_lists = []
     for text in text_file.readlines():
-        word_lists.append([SOS] + tokenizer(text.strip()) + [EOS])
+        word_lists.append([SOS] + fair_tokenizer(text.strip()) + [EOS])
     return word_lists
 
 def word_lists2numpy(word_lists, word2index):
@@ -12,12 +15,30 @@ def word_lists2numpy(word_lists, word2index):
     max_len = 0
     for word_list in word_lists:
         max_len = max(max_len, len(word_list))
-    max_index = 0
-    min_index = 30000
     for i in range(num):
         word_lists[i] = list(map(lambda x: word2index[x] if x in word2index else word2index[UNK], word_lists[i]))
         word_lists[i].extend([0] * (max_len - len(word_lists[i])))
-        max_index = max(max_index, max(word_lists[i]))
-        min_index = min(min_index, min(word_lists[i]))
-    print(max_index, min_index)
     return np.array(word_lists)
+
+def parse_path(base_path):
+    return {
+        'raw': {
+            'src_train': os.path.join(base_path, 'raw/src_train.txt'),
+            'trg_train': os.path.join(base_path, 'raw/trg_train.txt'),
+            'src_val': os.path.join(base_path, 'raw/src_val.txt'),
+            'trg_val': os.path.join(base_path, 'raw/trg_val.txt'),
+            'src_test': os.path.join(base_path, 'raw/src_test.txt'),
+            'trg_test': os.path.join(base_path, 'raw/trg_test.txt')
+        },
+        'processed': {
+            'src_train': os.path.join(base_path, 'processed/src_train.npz'),
+            'trg_train': os.path.join(base_path, 'processed/trg_train.npz'),
+            'src_val': os.path.join(base_path, 'processed/src_val.npz'),
+            'trg_val': os.path.join(base_path, 'processed/trg_val.npz'),
+            'src_test': os.path.join(base_path, 'processed/src_test.npz'),
+            'trg_test': os.path.join(base_path, 'processed/trg_test.npz')
+        },
+        'log': {
+            'data_log': os.path.join(base_path, 'log/data_log.yml')
+        }
+    }
